a8a599e9f2fdfd1921b1a9654aa258464b87e55e
Unknown <corentin.leroy@epitech.eu>
2019-01-26 14:37:32 +0100

    Created inventory script.

---
+++ Home Breaker/Assets/Scripts/Inventory/OpenInventory.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenInventory : MonoBehaviour
+{
+    public GameObject cube;
+    private bool state = false;
+    static private int scale = 2;
+    static private int objectSize = 32 * scale;
+    static private int shift = 10;
+    private Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cube = this.gameObject;
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var pos = cam.WorldToScreenPoint(cube.transform.position);
+        cube.transform.position = cam.ScreenToWorldPoint(new Vector3(objectSize / 2 + shift, objectSize / 2 + shift, pos.z));
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            SwitchVisibility();
+        }
+    }
+
+    void SwitchVisibility()
+    {
+        state = !state;
+        int i = 0;
+        Vector3 pos = cam.WorldToScreenPoint(this.transform.position);
+        foreach (Transform child in transform)
+        {
+            SetPosition(child, i, pos);
+            child.gameObject.SetActive(state);
+            i++;
+        }
+        Debug.Log("push");
+    }
+
+    void SetPosition(Transform child, int i, Vector3 pos)
+    {
+        var cpos = child.position;
+        Debug.Log(pos);
+        Vector3 newPos = cam.ScreenToWorldPoint(new Vector3(pos.x + i * (objectSize + 2 * shift), pos.y, pos.z));
+        child.transform.position = newPos;
+        Debug.Log("done");
+    }
+}
