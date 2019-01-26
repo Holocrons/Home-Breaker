d0ce38ebb4c5dd5113ae0c2fec45912de545b16d
Holocrons <remi.wickuler@epitech.eu>
2019-01-26 13:04:08 +0100

    small things

--- Home Breaker/Assets/DynamicMenu.cs
+++ Home Breaker/Assets/DynamicMenu.cs
@@ -7,12 +7,17 @@ public class DynamicMenu : MonoBehaviour
     public GameObject obj = null;
     private TextMesh[] tm;
     private int nb = 0;
+    private int count = 0;
     public GameObject arrow;
 
     // Start is called before the first frame update
     void Start()
     {
+        MeshRenderer[] mr;
         tm = GetComponentsInChildren<TextMesh>();
+        mr = GetComponentsInChildren<MeshRenderer>();
+        mr[0].sortingOrder = 4;
+        mr[1].sortingOrder = 4;
         UpdateData();
     }
 
@@ -23,12 +28,25 @@ public class DynamicMenu : MonoBehaviour
         {
             UpdateData();
         }
+        if (Input.GetKeyDown(KeyCode.S) && count < nb - 1)
+        {
+            arrow.transform.position = new Vector2(arrow.transform.position.x, arrow.transform.position.y - 1);
+            count++;
+        }
+        if (Input.GetKeyDown(KeyCode.Z) && count > 0)
+        {
+            arrow.transform.position = new Vector2(arrow.transform.position.x, arrow.transform.position.y + 1);
+            count--;
+        }
     }
 
     void UpdateData()
     {
         tm[0].text = obj.name;
         tm[1].text = "";
+        nb = 0;
+        arrow.transform.position = new Vector2(arrow.transform.position.x, arrow.transform.position.y + count);
+        count = 0;
         foreach (string str in obj.GetComponent<Item>().actions)
         {
             tm[1].text += str + "\n";
