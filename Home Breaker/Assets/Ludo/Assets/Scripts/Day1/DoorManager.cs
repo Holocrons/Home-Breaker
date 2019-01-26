02c8942581f67abff98676e922cf3dc00f61333e
Ludovic <ludovic.leuzzi@epitech.eu>
2019-01-26 14:47:05 +0100

    Menu + text

---
+++ Home Breaker/Assets/Ludo/Assets/Scripts/Day1/DoorManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorManager : MonoBehaviour
+{
+    public GameObject otherDoor;
+    public int destination;
+    public bool e = false;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Player" && e == true && Input.GetKeyDown(KeyCode.E))
+        {
+            collision.transform.position = otherDoor.transform.position;
+            collision.GetComponent<PlayerMovement>().currentsScene = destination;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player" && e == false && collision.GetComponent<PlayerMovement>().timer <= Time.time)
+        {
+            collision.transform.position = otherDoor.transform.position;
+            collision.GetComponent<PlayerMovement>().currentsScene = destination;
+            collision.GetComponent<PlayerMovement>().timer = Time.time + 0.1f;
+        }
+    }
+}
