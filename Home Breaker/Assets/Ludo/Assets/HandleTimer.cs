69f74ba1897beb4a9848e4b6d8ead40556f8fb87
Ludovic <ludovic.leuzzi@epitech.eu>
2019-01-27 07:51:37 +0100

    asset

---
+++ Home Breaker/Assets/Ludo/Assets/HandleTimer.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HandleTimer : MonoBehaviour
+{
+    private Text uiText;
+    private float mainTimer;
+
+    private float timer;
+    private bool canCount = true;
+    private bool doOnce = false;
+
+    private void Start()
+    {
+        timer = 300;
+    }
+
+    private void Update()
+    {
+        if (timer >= 0.0f && canCount)
+        {
+            timer -= Time.deltaTime;
+            GetComponent<Text> ().text = string.Format("{0:0}:{1:00}", Mathf.Floor(timer / 60), timer % 60);
+        }
+        else if (timer <= 0.0F && !doOnce)
+        {
+            canCount = false;
+            doOnce = true;
+            GetComponent<Text> ().text = "0.00";
+            timer = 0.0F;
+            ChangeScene();
+        }
+    }
+
+    public void ResetBtn()
+    {
+        timer = mainTimer;
+        canCount = true;
+        doOnce = false;
+    }
+
+    void ChangeScene()
+    {
+        
+    }
+}
